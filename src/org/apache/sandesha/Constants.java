460f21d7626aa0cb33edbe98c3e836518347868c
Sudar Nimalan <snimalan@apache.org>
2004-05-04 11:28:32 +0000

    needed constants in RMClientService PR: Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: PR: CVS:   If this change addresses a PR in the problem report tracking CVS:   database, then enter the PR number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, such as NCSA, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to Apache by someone else; i.e., CVS:   they sent us a patch or a new module, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/sandesha/trunk@224744 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/sandesha/Constants.java
+++ src/org/apache/sandesha/Constants.java
@@ -26,84 +26,108 @@ package org.apache.sandesha;
  */
 public interface Constants {
 
-    /**
-     * Field RM_CLIENT_SERVICE
-     */
-    public static final String RM_CLIENT_SERVICE = "RMClientService";
+	/**
+	 * Field RM_CLIENT_SERVICE
+	 */
+	public static final String RM_CLIENT_SERVICE = "RMClientService";
 
-    /**
-     * Field CLIENT_METHOD
-     */
-    public static final String CLIENT_METHOD = "clientMethod";
+	/**
+	 * Field CLIENT_METHOD
+	 */
+	public static final String CLIENT_METHOD = "clientMethod";
 
-    /**
-     * Field AXIS_SERVICES
-     */
-    public static final String AXIS_SERVICES = "/axis/services/";
+	/**
+	 * Field AXIS_SERVICES
+	 */
+	public static final String AXIS_SERVICES = "/axis/services/";
 
-    /**
-     * Field QUESTION_WSDL
-     */
-    public static final String QUESTION_WSDL = "?wsdl";
+	/**
+	 * Field QUESTION_WSDL
+	 */
+	public static final String QUESTION_WSDL = "?wsdl";
 
-    /**
-     * Field CLIENT_REFERANCE
-     */
-    public static final String CLIENT_REFERANCE = "RMClientReferance";
+	/**
+	 * Field CLIENT_REFERANCE
+	 */
+	public static final String CLIENT_REFERANCE = "RMClientReferance";
 
-    // Policy related constants.
-    // public static final  EXPIRATION=new Date();
+	// Policy related constants.
+	// public static final  EXPIRATION=new Date();
 
-    /**
-     * Field INACTIVITY_TIMEOUT
-     */
-    public static final long INACTIVITY_TIMEOUT =
-            60000;    // double the expectd for breaking of the network in ms.
+	/**
+	 * Field INACTIVITY_TIMEOUT
+	 */
+	public static final long INACTIVITY_TIMEOUT = 60000;
+	// double the expectd for breaking of the network in ms.
 
-    /**
-     * Field RETRANSMISSION_INTERVAL
-     */
-    public static final long RETRANSMISSION_INTERVAL = 2000;    // Set to two 2000ms
+	/**
+	 * Field RETRANSMISSION_INTERVAL
+	 */
+	public static final long RETRANSMISSION_INTERVAL = 2000;
+	// Set to two 2000ms
 
-    /**
-     * Field MAXIMUM_RETRANSMISSION_COUNT
-     */
-    public static final int MAXIMUM_RETRANSMISSION_COUNT = 20;
+	/**
+	 * Field MAXIMUM_RETRANSMISSION_COUNT
+	 */
+	public static final int MAXIMUM_RETRANSMISSION_COUNT = 20;
 
-    /**
-     * Field ANONYMOUS_URI
-     */
-    public static final String ANONYMOUS_URI =
-            "http://schemas.xmlsoap.org/ws/2003/03/addressing/role/anonymous";
+	/**
+	 * Field ANONYMOUS_URI
+	 */
+	public static final String ANONYMOUS_URI =
+		"http://schemas.xmlsoap.org/ws/2003/03/addressing/role/anonymous";
 
-    /**
-     * Field NS_PREFIX_RM
-     */
-    public static final String NS_PREFIX_RM = "wsrm";
+	/**
+	 * Field NS_PREFIX_RM
+	 */
+	public static final String NS_PREFIX_RM = "wsrm";
 
-    /**
-     * Field NS_URI_RM
-     */
-    public static final String NS_URI_RM = "http://schemas.xmlsoap.org/ws/2004/03/rm";
+	/**
+	 * Field NS_URI_RM
+	 */
+	public static final String NS_URI_RM =
+		"http://schemas.xmlsoap.org/ws/2004/03/rm";
 
-    /**
-     * Field WSU_PREFIX
-     */
-    public static final String WSU_PREFIX = "wsu";
+	/**
+	 * Field WSU_PREFIX
+	 */
+	public static final String WSU_PREFIX = "wsu";
 
-    /**
-     * Field WSU_NS
-     */
-    public static final String WSU_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
+	/**
+	 * Field WSU_NS
+	 */
+	public static final String WSU_NS =
+		"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
 
-    /**
-     * Field MAX_CHECKING_TIME
-     */
-    public static final int MAX_CHECKING_TIME = 2;
+	/**
+	 * Field MAX_CHECKING_TIME
+	 */
+	public static final int MAX_CHECKING_TIME = 2;
 
-    /**
-     * Field ENV_RM_REQUEST_HEADERS
-     */
-    public static final String ENV_RM_REQUEST_HEADERS =
-            "org.apache.sandesha.ws.rm.REQUEST.HEADERS";
+	/**
+	 * Field ENV_RM_REQUEST_HEADERS
+	 */
+	public static final String ENV_RM_REQUEST_HEADERS =
+		"org.apache.sandesha.ws.rm.REQUEST.HEADERS";
+
+	/**
+	 * Field CLIENT_SEQUENCE_IDENTIFIER
+	 */
+	public static final String CLIENT_SEQUENCE_IDENTIFIER = "SequenceIdetifier";
+	/**
+	 * Field CLIENT_ONE_WAY_INVOKE
+	 */
+	public static final String CLIENT_ONE_WAY_INVOKE = "OneWayInvoke";
+	/**
+	 * Field CLIENT_RESPONSE_EXPECTED
+	 */
+	public static final String CLIENT_RESPONSE_EXPECTED = "ResponseExpected";
+	/**
+	 * Field CLIENT_CREATE_SEQUENCE
+	 */
+	public static final String CLIENT_CREATE_SEQUENCE = "CreateSequence";
+	/**
+	 * Field CLIENT_LAST_MESSAGE
+	 */
+	public static final String CLIENT_LAST_MESSAGE = "LastMessage";
 }
