38ed76f1c4927e69905ff24f3ecb25dc1c294f50
Sudar Nimalan <snimalan@apache.org>
2004-05-07 08:26:04 +0000

    This is the sample Client for Scenario 2.1 PR: Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: PR: CVS:   If this change addresses a PR in the problem report tracking CVS:   database, then enter the PR number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, such as NCSA, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to Apache by someone else; i.e., CVS:   they sent us a patch or a new module, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/sandesha/trunk@224760 13f79535-47bb-0310-9956-ffa450edef68

---
+++ interop/org/apache/sandesha/samples/interop/Scenario_2_1_Client.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright  1999-2004 The Apache Software Foundation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.sandesha.samples.interop;
+
+import javax.xml.namespace.QName;
+import javax.xml.rpc.ParameterMode;
+
+import org.apache.axis.client.Call;
+import org.apache.axis.client.Service;
+import org.apache.axis.encoding.XMLType;
+import org.apache.sandesha.Constants;
+
+/**
+ * Run client for
+ * <a href="http://www-106.ibm.com/developerworks/offers/WS-Specworkshops/ws-rm200405.html">interop scenario 2.1</a>
+ *
+ * @author Aleksander Slominski
+ * @author Amila Navarathna
+ * @author Jaliya Ekanayaka
+ * @author Sudar Nimalan
+ */
+
+public class Scenario_2_1_Client {
+	public static void main(String[] args) {
+		System.out.println("Client started......");
+		try {
+
+			Service service = new Service();
+			Call call = (Call) service.createCall();
+			if (args.length == 0 || args[0].equals("")) {
+				throw new Exception("Error: pass Target End Point Address as a Parametter");
+			}
+			call.setTargetEndpointAddress(args[0]);
+			call.setOperationName(new QName("PingService", "Ping"));
+			call.addParameter("Text", XMLType.XSD_STRING, ParameterMode.IN);
+			call.setReturnType(XMLType.AXIS_VOID);
+
+			call.setProperty(
+				Constants.CLIENT_RESPONSE_EXPECTED,
+				(new Boolean(false)));
+
+			call.invoke(new Object[] { "Ping 1" });
+			call.invoke(new Object[] { "Ping 2" });
+			call.setProperty(
+				Constants.CLIENT_LAST_MESSAGE,
+				(new Boolean(false)));
+			call.invoke(new Object[] { "Ping 3" });
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
