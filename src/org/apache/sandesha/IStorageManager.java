770e6da9506f19d4f22930c11c6b8e1ba6d0c4bb
Jaliya Ekanayake <jaliya@apache.org>
2004-12-24 04:46:59 +0000

    isResponseComplete()  method is added, This is to check whether we have all the responses received
    git-svn-id: https://svn.apache.org/repos/asf/webservices/sandesha/trunk@224882 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/sandesha/IStorageManager.java
+++ src/org/apache/sandesha/IStorageManager.java
@@ -114,4 +114,10 @@ public interface IStorageManager {
     public boolean isAckComplete(String sequenceID);
     public void insertTerminateSeqMessage(RMMessageContext terminateSeqMessage);
     public boolean isAllSequenceComplete();
+
+    /**
+     * @param sequenceID
+     * @return
+     */
+    public boolean isResponseComplete(String sequenceID);
 }
