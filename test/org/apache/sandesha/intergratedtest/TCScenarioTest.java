510f78d78123812a6f21cc0dacb3d4b477fe8cfe
Jaliya Ekanayake <jaliya@apache.org>
2005-05-20 10:52:26 +0000

    A test case for scenario testing
    git-svn-id: https://svn.apache.org/repos/asf/webservices/sandesha/trunk@225043 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/org/apache/sandesha/intergratedtest/TCScenarioTest.java
@@ -0,0 +1,11 @@
+package org.apache.sandesha.intergratedtest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jaliya
+ * Date: May 20, 2005
+ * Time: 4:47:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TCScenarioTest {
+}
