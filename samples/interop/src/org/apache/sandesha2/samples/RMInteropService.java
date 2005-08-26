2f23e2a9d1072840b47aa8e053c5ebdbb4bfa9a7
Chamikara Madhusanka Jayalath <chamikara@apache.org>
2005-08-26 10:06:09 +0000

    maven goals were developed to create sandesha module and sample interop service.
    Several classes were added.
    
    git-svn-id: https://svn.apache.org/repos/asf/webservices/sandesha/trunk@240199 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/interop/src/org/apache/sandesha2/samples/RMInteropService.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright  1999-2004 The Apache Software Foundation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.apache.sandesha2.samples;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 
+ * 
+ */
+public class RMInteropService {
+
+    private static Map sequences = new HashMap();
+
+    public String echoString(String text, String sequence) {
+
+        if (sequences.get(sequence) != null) {
+            text = (String) sequences.get(sequence) + text;
+            sequences.put(sequence, new String(text));
+        } else {
+            sequences.put(sequence, (new String(text)));
+
+        }
+        return text;
+    }
+
+    public void ping(String text) {
+        //Just accept the message and do some processing
+    }
+}
