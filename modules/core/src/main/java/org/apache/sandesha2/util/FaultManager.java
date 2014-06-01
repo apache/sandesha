04dcbd0f0f2270e02ef52e1c0f19f46a862a63c7
Andreas Veithen <veithen@apache.org>
2014-06-01 07:39:09 +0000

    Merged r1598950 to the 1.6 branch for compatibility with future Axiom versions.
    git-svn-id: https://svn.apache.org/repos/asf/axis/axis2/java/sandesha/branches/1_6@1598952 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/main/java/org/apache/sandesha2/util/FaultManager.java
+++ modules/core/src/main/java/org/apache/sandesha2/util/FaultManager.java
@@ -560,8 +560,6 @@ public class FaultManager {
 		faultCode.setSubCode(faultSubCode);
 		
 		SOAPFaultReason reason = factory.createSOAPFaultReason();
-		SOAPFaultText reasonText = factory.createSOAPFaultText();
-		reasonText.setText(data.getReason());
 		
 		SOAPFaultDetail detail = factory.createSOAPFaultDetail();
 		detail.addDetailEntry(data.getDetail());
@@ -570,6 +568,8 @@ public class FaultManager {
 		
 		if (SOAP12Constants.SOAP_ENVELOPE_NAMESPACE_URI.equals(SOAPNamespaceValue)) 
 		{
+            SOAPFaultText reasonText = factory.createSOAPFaultText();
+            reasonText.setText(data.getReason());
             reasonText.setLang(Sandesha2Constants.LANG_EN);
 			reason.addSOAPText(reasonText);
 			referenceRMMsgContext.setProperty(SOAP12Constants.SOAP_FAULT_CODE_LOCAL_NAME, faultCode);
